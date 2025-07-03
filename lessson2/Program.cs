6361692279ba10d4a143c9701341d01718d76d23
idil12356 <idlmhad@gmail.com>
2025-07-03 12:44:44 +0100

    mini project (student quiz system)

--- lessson2/Program.cs
+++ lessson2/Program.cs
@@ -10,51 +10,86 @@ namespace lessson2
     {
         static void Main(string[] args)
         {
-            string Name, Pob, Dob, Email, Status, Sex, tell;
-            
+            Console.Write("Geli Magacaaga: ");
+            string name = Console.ReadLine();
 
-            Console.WriteLine("Gali magaca");
-            Name = Console.ReadLine();
-            Console.WriteLine(Name);
+            Console.Write("Geli ID gaaga: ");
+            string id = Console.ReadLine();
 
-            Console.WriteLine("Gali mesha kudalatay");
-            Pob = Console.ReadLine();
-            Console.WriteLine(Pob);
+            Console.WriteLine("\n Dooro nooca xisaabta:");
+            Console.WriteLine("1. Kudar");
+            Console.WriteLine("2. Kajar");
+            Console.WriteLine("3. Kudhufo");
+            Console.WriteLine("4. Qaybi");
+            Console.Write("Dooro (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Gali Taariikhda dhalatay");
-            Dob = Console.ReadLine();
-            Console.WriteLine(Dob);
+            int correct = 0;
+            int total = 10;
 
-            Console.WriteLine("Gali Emailkaaga");
-            Email = Console.ReadLine();
-            Console.WriteLine(Email);
+            Random rand = new Random();
 
-            Console.WriteLine("Gali Statuskaaga");
-            Status = Console.ReadLine();
-            Console.WriteLine(Status);
-
-            Console.WriteLine("Gali sex gaaga");
-            Sex = Console.ReadLine();
-            Console.WriteLine(Sex);
-
-            Console.WriteLine("Gali Lambarkaaga");
-            tell = Console.ReadLine();
-            Console.WriteLine(tell);
-
-
-            Random Qulxin = new Random();
-            int tiro;
-
-            int iskudar = 0;
-            int loop;
-            for(loop = 1; loop <= 10; loop++)
+            for (int i = 1; i <= total; i++)
             {
-                tiro = Qulxin.Next(1, 50);
-                Console.WriteLine(loop+"\t\t"+tiro);
-                iskudar = iskudar + tiro;
+                int a = rand.Next(1, 20);
+                int b = rand.Next(1, 20);
+                int answer = 0;
+                int userAnswer = 0;
 
+                switch (choice)
+                {
+                    case 1: // Kudar
+                        Console.Write($"Q{i}: {a} + {b} = ");
+                        answer = a + b;
+                        break;
+                    case 2: // Kajar
+                        Console.Write($"Q{i}: {a} - {b} = ");
+                        answer = a - b;
+                        break;
+                    case 3: // Kudhufo
+                        Console.Write($"Q{i}: {a} * {b} = ");
+                        answer = a * b;
+                        break;
+                    case 4: // Qaybi
+                        a = a * b; // si b ay u qaybin karto a
+                        Console.Write($"Q{i}: {a} / {b} = ");
+                        answer = a / b;
+                        break;
+                    default:
+                        Console.WriteLine(" Doorasho khaldan.");
+                        return;
+                }
+
+                try
+                {
+                    userAnswer = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine(" Kaliya tiro gelin.");
+                    continue;
+                }
+
+                if (userAnswer == answer)
+                {
+                    Console.WriteLine(" Sax!\n");
+                    correct++;
+                }
+                else
+                {
+                    Console.WriteLine($" Khalad. Jawaabta saxda ah waa: {answer}\n");
+                }
             }
-            Console.WriteLine("iskudarka waa" + iskudar);
+
+            int wrong = total - correct;
+            int percent = (correct * 100) / total;
+
+            Console.WriteLine("===== Natiijada =====");
+            Console.WriteLine($" Magaca: {name}");
+            Console.WriteLine($" ID: {id}");
+            Console.WriteLine($" Sax: {correct}");
+            Console.WriteLine($" Khalad: {wrong}");
+            Console.WriteLine($" Boqolley: {percent}%");
         }
     }
 }
